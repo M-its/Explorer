6c4fc28b85e0bf5012ddd873ffac265a5e04fd5b
Mitsrael Souza <mitarael9@gmail.com>
2024-04-12 09:54:13 -0300

    add spa-universe

---
+++ spa-universe/js/index.js
@@ -0,0 +1,13 @@
+import { Router } from "./router.js"
+
+const router = new Router()
+
+router.add("/", "/pages/home.html", "home")
+router.add("/exploration", "/pages/exploration.html", "exploration")
+router.add("/universe", "/pages/universe.html", "universe")
+router.add(404, "/pages/404.html", "not-found")
+
+router.handle()
+
+window.onpopstate = () => router.handle()
+window.route = () => router.route()
---
+++ spa-universe/js/router.js
@@ -0,0 +1,33 @@
+export class Router {
+    routes = {}
+
+    add(routeName, page, styleClass, cssPath) {
+        this.routes[routeName] = { page, styleClass, cssPath }
+    }
+
+    route(event) {
+        event = event || window.event
+        event.preventDefault()
+
+        window.history.pushState({}, "", event.target.href)
+
+        this.handle()
+    }
+
+    handle() {
+        const { pathname } = window.location
+        const route = this.routes[pathname] || this.routes[404]
+
+        fetch(route.page)
+            .then((data) => data.text())
+            .then((html) => {
+                const app = document.querySelector("#app")
+                const page = document.querySelector("html")
+                const cssLink = document.querySelector("#page-css")
+
+                app.innerHTML = html
+                page.className = route.styleClass
+                cssLink.href = `pages/${route.styleClass}.css`
+            })
+    }
+}
