c81b95c1fefab1b318c73a32d0704ea11db42ee5
Mitsrael Souza <mitsrael9@gmail.com>
2024-04-23 10:31:55 -0300

    Update index.js

--- spa-universe/js/index.js
+++ spa-universe/js/index.js
@@ -2,7 +2,7 @@ import { Router } from "./router.js"
 
 const router = new Router()
 
-router.add("/", "./Explorer/spa-universe/pages/home.html", "home")
+router.add("/", "/pages/home.html", "home")
 router.add("/exploration", "./pages/exploration.html", "exploration")
 router.add("/universe", "./pages/universe.html", "universe")
 router.add(404, "./pages/404.html", "404")
